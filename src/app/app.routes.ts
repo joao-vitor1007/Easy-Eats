a8b06f082b0090fde96a4d7eb71648f60ccdda18
ZacML <isaquem673@gmail.com>
2026-03-28 22:56:09 -0300

    Tela "Perfil do Garçom"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,6 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Cadastro } from './cadastro/cadastro';
 import { DashboardComponent } from './dashboard/dashboard';
-import { PerfilAdmin } from './perfilAdmin/perfilAdmin';
-import { PerfilGarcom } from './perfilGarcom/perfilGarcom';
-
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -19,13 +16,5 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
-  },
-  {
-    path: 'perfil-admin',
-    component: PerfilAdmin,
-  },
-  {
-    path: 'perfil-garcom',
-    component: PerfilGarcom,
   }
 ];
